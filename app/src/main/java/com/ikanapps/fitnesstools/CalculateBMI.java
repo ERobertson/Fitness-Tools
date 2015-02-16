2b1b5790678d19c355882f4f8713ac6469c42b65
Eric Robertson <ericwaynerobertson@gmail.com>
2015-02-16 11:35:28 -0500

    Initial commit.

---
+++ app/src/main/java/com/ikanapps/fitnesstools/CalculateBMI.java
@@ -0,0 +1,84 @@
+package com.ikanapps.fitnesstools;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class CalculateBMI extends ActionBarActivity {
+    private Button btnCalculate;
+    private Button btnHome;
+    private EditText txtFeet;
+    private EditText txtInches;
+    private EditText txtWeight;
+    private TextView lblResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_bmi);
+        setHomeOnClick();
+        setCalculateOnClick();
+    }
+
+    private void setHomeOnClick(){
+        btnHome = (Button) findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+        public void onClick(View v){
+                Intent goHomeIntent = new Intent(CalculateBMI.this, MainActivity.class);
+                startActivity(goHomeIntent);
+            }
+        });
+    }
+
+    private void setCalculateOnClick() {
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float bmi;
+                float inches;
+                float weight;
+                txtInches = (EditText) findViewById(R.id.txtInches);
+                txtFeet = (EditText) findViewById(R.id.txtFeet);
+                txtWeight = (EditText) findViewById(R.id.txtWeight);
+                lblResult = (TextView) findViewById(R.id.lblResult);
+
+                weight = Float.valueOf(txtWeight.getText().toString());
+                inches = Float.valueOf(txtFeet.getText().toString()) * 12 + Float.valueOf(txtInches.getText().toString());
+                bmi = (weight / (inches * inches)) * 703;
+                lblResult.setText(String.valueOf(bmi));
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_calculate_bmi, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/ikanapps/fitnesstools/MaxHeartRate.java
@@ -0,0 +1,78 @@
+package com.ikanapps.fitnesstools;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MaxHeartRate extends ActionBarActivity {
+    private Button btnHome;
+    private Button btnCalculate;
+    private TextView lblResult;
+    private EditText txtAge;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_max_heart_rate);
+
+        setHomeOnClick();
+        setCalculateOnClick();
+    }
+
+    private void setHomeOnClick(){
+        btnHome = (Button) findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent goHomeIntent = new Intent(MaxHeartRate.this, MainActivity.class);
+                startActivity(goHomeIntent);
+            }
+        });
+    }
+
+    private void setCalculateOnClick(){
+        btnCalculate = (Button) findViewById(R.id.btnCalculate);
+        txtAge = (EditText) findViewById(R.id.txtAge);
+        lblResult = (TextView) findViewById(R.id.lblResult);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener(){
+            @Override
+        public void onClick(View v){
+                Integer maxRate;
+                maxRate = 220 - Integer.valueOf(txtAge.getText().toString());
+                //Toast.makeText(MaxHeartRate.this, R.string.mhrResult + String.valueOf(maxRate), Toast.LENGTH_LONG).show();
+                lblResult.setText("Your max heart rate is: " + String.valueOf(maxRate));
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_max_heart_rate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
